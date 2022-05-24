25f0153db16c7bc95966a556123b960d270e0851
gabriel-cangucu <gabriel.lima.cangucu@gmail.com>
2022-05-23 23:47:33 -0300

    Primeiros métodos adicionadas

--- src/graphex.py
+++ src/graphex.py
@@ -1,7 +1,14 @@
 class Graph():
-    def __init__(self, weighted=False) -> None:
+    def __init__(self, weighted=False, directed=False) -> None:
+        '''
+        Our graph is a dictionary of tuples, in which each node 'u' points to
+        a list of tuples in the form (v, w), where 'v' is a node and 'w' is the
+        the wight of the edge (u, v)
+        '''
         self._G = dict()
-        self._weighted = weighted
+
+        self._weighted = weighted   # If not weighted, every edge receives weight 1
+        self._directed = directed   # If directed, both (u, v) and (v, u) are added to G
     
     def get_nodes(self) -> list:
         return self._G.keys()
@@ -9,8 +16,51 @@ class Graph():
     def get_edges(self) -> list:
         edges = []
 
-        for node in self.get_nodes():
-            edges.extend(self._G[node])
+        for u in self.get_nodes():
+            for v, w in self.get_adjacency_list(u):
+                if self._weighted:
+                    edge = (u, v, w)
+                else:
+                    edge = (u, v)
+
+                edges.append(edge)
         
         # Removing duplicates by turning the list into a set
         return list(set(edges))
+    
+    def get_adjacency_list(self, node) -> list:
+        if node in self.get_nodes():
+            return self._G[node]
+        else:
+            raise KeyError('{node} is not a node of the graph')
+
+    def add_nodes(self, nodes) -> None:
+        # If only one node is provided, we turn it into a list
+        if not isinstance(nodes, list):
+            nodes = [nodes]
+
+        for node in nodes:
+            self._G[node] = []
+    
+    def add_edges(self, edges) -> None:
+        # If only one edge is provided, we turn it into a list
+        if not isinstance(edges, list):
+            edges = [edges]
+
+        for edge in edges:
+            if self._weighted:
+                u, v, w = edge
+
+            else:
+                u, v = edge
+                w = 1
+            
+            if u not in self.get_nodes():
+                self.add_nodes(u)
+            self._G[u].append((v, w))
+
+            # An undirected graph has both edges (u, v) and (v, u)
+            if not self._directed:
+                if v not in self.get_nodes():
+                    self.add_nodes(v)
+                self._G[v].append((u, w))
--- tests/test_graphex.py
+++ tests/test_graphex.py
@@ -4,15 +4,77 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import unittest
 import src.graphex as gx
 
+
 class TestGraphex(unittest.TestCase):
     def setUp(self):
         self.G = gx.Graph()
 
     def test_graph_initialized_with_no_nodes(self):
-        self.assertEqual(len(self.G.get_nodes()), 0)
+        nodes = self.G.get_nodes()
+        self.assertEqual(len(nodes), 0)
 
     def test_graph_initialized_with_no_edges(self):
-        self.assertEqual(len(self.G.get_edges()), 0)
+        edges = self.G.get_edges()
+        self.assertEqual(len(edges), 0)
+    
+    def test_adding_one_node(self):
+        self.G.add_nodes(10)
+        nodes = self.G.get_nodes()
+
+        self.assertEqual(len(nodes), 1)
+        self.assertIn(10, nodes)
+    
+    def test_adding_multiple_nodes(self):
+        self.G.add_nodes([10, 'a', 3.14])
+        nodes = self.G.get_nodes()
+
+        self.assertEqual(len(nodes), 3)
+        self.assertIn('a', nodes)
+    
+    def test_new_node_has_no_neighbors(self):
+        self.G.add_nodes(10)
+        adj_list = self.G.get_adjacency_list(10)
+
+        self.assertEqual(len(adj_list), 0)
+    
+    def test_get_adjacency_list_from_inexistent_node(self):
+        with self.assertRaises(KeyError) as context:
+            _ = self.G.get_adjacency_list(10)
+
+        self.assertTrue('not a node' in str(context.exception))
+    
+    def test_adding_one_edge_to_undirected_graph(self):
+        self.G.add_edges((10, 20))
+        edges = self.G.get_edges()
+
+        self.assertIn((10, 20), edges)
+        self.assertIn((20, 10), edges)
+    
+    def test_adding_one_edge_to_directed_graph(self):
+        G = gx.Graph(directed=True)
+        G.add_edges((10, 20))
+        edges = G.get_edges()
+
+        self.assertIn((10, 20), edges)
+        self.assertNotIn((20, 10), edges)
+
+    def test_adding_multiple_edges(self):
+        self.G.add_edges([(10, 20), (20, 30)])
+        edges = self.G.get_edges()
+
+        self.assertIn((10, 20), edges)
+        self.assertIn((20, 30), edges)
+    
+    def test_adding_an_edge_creates_nodes(self):
+        self.G.add_edges((10, 20))
+        nodes = self.G.get_nodes()
+
+        self.assertIn(10, nodes)
+        self.assertIn(20, nodes)
+    
+    def test_edge_has_weight_one_if_unweighted_graph(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
